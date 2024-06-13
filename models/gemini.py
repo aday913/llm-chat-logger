39e84db99070ed2865d178331094f0ee8436d4d4
Alex Day <alexdaygjj@gmail.com>
2024-06-13 01:37:32 -0700

    Enable conversation-style llm prompting

--- models/gemini.py
+++ models/gemini.py
@@ -16,8 +16,6 @@ class Gemini_Model:
         genai.configure(api_key=api_key)
         self.gemini = genai.GenerativeModel(model)
 
-        self.message_history = []
-
     def generate_text(self, prompt: str) -> str | None:
         """
         Generate text based on a prompt
@@ -29,7 +27,7 @@ class Gemini_Model:
         completion = self.gemini.generate_content(prompt)
         return completion.text
 
-    def converse(self, messages: list) -> str | None:
+    def converse(self, message_history: list = []) -> list:
         """
         Converse with the model in a conversation-like pattern
 
@@ -37,9 +35,64 @@ class Gemini_Model:
 
         :return: the model's response
         """
-        completion = self.gemini.generate_content(messages)
-        self.message_history.append({"role": "model", "parts": [completion.text]})
+        while True:
+            user_prompt = self.get_user_input()
+
+            if user_prompt is None:
+                return message_history
+
+            message_history.append({"role": "user", "parts": [user_prompt]})
+
+            llm_response = self.get_model_response(message_history)
+
+            print(llm_response)
+
+            message_history.append({"role": "model", "parts": [llm_response]})
+
+    def get_user_input(self):
+        """
+        Get user input
+
+        :return: the user input
+        """
+        message = """"""
+        num_newlines = 0
+        continue_conversation = True
+        while num_newlines < 2:
+            user_input = input(": ")
+            if user_input.strip() == "exit" or user_input.strip() == "quit":
+                continue_conversation = False
+                break
+            elif user_input == "":
+                num_newlines += 1
+                # print(num_newlines)
+            else:
+                message += user_input + "\n"
+
+        if continue_conversation:
+            return message
+        else:
+            return None
+
+    def get_model_response(self, messages: list) -> str | None:
+        """
+        Get the model's response
+
+        :param messages: the messages to send to the model
+
+        :return: the model's response
+        """
+        completion = self.gemini.generate_content(
+            messages
+        )
+
         return completion.text
+
+    def run(self):
+        """
+        Run the model
+        """
+        _ = self.converse()
 
 
 if __name__ == "__main__":
@@ -56,9 +109,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # initialize model
-    gpt = Gemini_Model(model=args.model, api_key=API_KEY)
+    gemini = Gemini_Model(model=args.model, api_key=API_KEY)
 
     # generate try:
-    prompt = input("Enter a prompt: ")
+    # prompt = input("Enter a prompt: ")
+    #
+    # print(gpt.generate_text(prompt))
 
-    print(gpt.generate_text(prompt))
+    gemini.run()
