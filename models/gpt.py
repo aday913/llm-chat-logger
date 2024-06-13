a794c5bd1c987d7132eb9049e2291f80089378b6
Alex Day <alexdaygjj@gmail.com>
2024-06-13 01:30:46 -0700

    Implement conversation-style prompting with gpt

--- models/gpt.py
+++ models/gpt.py
@@ -16,8 +16,6 @@ class GPT_Model:
         self.openai = OpenAI(api_key=api_key)
         self.model = model
 
-        self.message_history = []
-
     def generate_text(self, prompt: str) -> str | None:
         """
         Generate text based on a prompt
@@ -35,7 +33,7 @@ class GPT_Model:
         )
         return completion.choices[0].message.content
 
-    def converse(self, messages: list) -> str | None:
+    def converse(self, message_history: list = []) -> list:
         """
         Converse with the model in a conversation-like pattern
 
@@ -43,13 +41,64 @@ class GPT_Model:
 
         :return: the model's response
         """
+        while True:
+            user_prompt = self.get_user_input()
+
+            if user_prompt is None:
+                return message_history
+
+            message_history.append({"role": "user", "content": user_prompt})
+
+            llm_response = self.get_model_response(message_history)
+
+            print(llm_response)
+
+            message_history.append({"role": "assistant", "content": llm_response})
+
+    def get_user_input(self):
+        """
+        Get user input
+
+        :return: the user input
+        """
+        message = """"""
+        num_newlines = 0
+        continue_conversation = True
+        while num_newlines < 2:
+            user_input = input(": ")
+            if user_input.strip() == "exit" or user_input.strip() == "quit":
+                continue_conversation = False
+                break
+            elif user_input == "":
+                num_newlines += 1
+                # print(num_newlines)
+            else:
+                message += user_input + "\n"
+
+        if continue_conversation:
+            return message
+        else:
+            return None
+
+    def get_model_response(self, messages: list) -> str | None:
+        """
+        Get the model's response
+
+        :param messages: the messages to send to the model
+
+        :return: the model's response
+        """
         completion = self.openai.chat.completions.create(
             model=self.model, messages=messages
         )
-        self.message_history.append(
-            {"role": "assistant", "content": completion.choices[0].message.content}
-        )
+
         return completion.choices[0].message.content
+
+    def run(self):
+        """
+        Run the model
+        """
+        _ = self.converse()
 
 
 if __name__ == "__main__":
@@ -58,7 +107,9 @@ if __name__ == "__main__":
     load_dotenv()
 
     # get api key
-    API_KEY: str = os.getenv("OPENAI_API_KEY")
+    API_KEY: str | None = os.getenv("OPENAI_API_KEY")
+    if API_KEY is None:
+        raise ValueError("API key not found in .env file")
 
     # parse arguments
     parser = argparse.ArgumentParser()
@@ -69,6 +120,8 @@ if __name__ == "__main__":
     gpt = GPT_Model(model=args.model, api_key=API_KEY)
 
     # generate try:
-    prompt = input("Enter a prompt: ")
+    # prompt = input("Enter a prompt: ")
+    #
+    # print(gpt.generate_text(prompt))
 
-    print(gpt.generate_text(prompt))
+    gpt.run()
