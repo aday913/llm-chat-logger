bb5bcb211d8bcb8fc45e73a63ab34cb69ba86ea0
Alex Day <alexdaygjj@gmail.com>
2024-06-19 20:14:00 -0700

    Save conversation to .md file, use prompt text as context

--- main.py
+++ main.py
@@ -17,13 +17,29 @@ LLM_MODELS = [
 ]
 
 
+def save_conversation_to_file(conversation: list, output: str):
+    with open(output, "w") as f:
+        for talker, message in conversation:
+            f.write(f"*{str(talker).capitalize()}*:\n\n")
+            f.write(f"{message}\n\n")
+            f.write("---\n\n")
+
+
+def get_prompt_text(prompt: str) -> str:
+    text = """"""
+    with open(f"prompts/{prompt}.md", "r") as f:
+        for line in f:
+            text += line.strip() + "\n"
+    return text
+
+
 def main(
     openai_key: str | None,
     gemini_key: str | None,
     model: str,
     prompt: str,
     output: str,
-    continue_file: str,
+    continue_file: str | None,
 ):
     if model == "gpt":
         model = "gpt-4o"
@@ -40,11 +56,29 @@ def main(
     else:
         raise ValueError("API key not found")
 
-    llm.run()
+    # load prompt
+    prompt_text = get_prompt_text(prompt)
+
+    # load previous conversation, if any
+
+    conversation = llm.converse(prompt=prompt_text)
+    for talker, message in conversation:
+        print(f"{talker.capitalize()}: {len(message)} characters")
+
+    # save conversation to file
+    save_conversation_to_file(conversation, output)
 
 
 if __name__ == "__main__":
     load_dotenv()
+
+    output_path = os.getenv("DEFAULT_DIR")
+    if output_path is None:
+        raise FileNotFoundError("Output directory not found in .env file")
+    if not os.path.exists(output_path):
+        raise FileNotFoundError(
+            f"Configured output directory '{output_path}' not found"
+        )
 
     parser = argparse.ArgumentParser(
         description="Communicate with an LLM model and save the conversation to a markdown file"
@@ -53,7 +87,7 @@ if __name__ == "__main__":
         "-m",
         "--model",
         type=str,
-        help="The model to use",
+        help="The model to use (gpt-4o, gpt-4-turbo, gpt-4, gpt-3.5-turbo, gemini-1.5-pro, gemini-1.5-flash, gemini-1.0-pro)",
         required=False,
         default="gpt-4o",
     )
@@ -61,7 +95,7 @@ if __name__ == "__main__":
         "-p",
         "--prompt",
         type=str,
-        help="The prompt name to start the conversation",
+        help="The prompt name to start the conversation (without the .md extension)",
         required=False,
         default="default_ai",
     )
@@ -71,7 +105,7 @@ if __name__ == "__main__":
         type=str,
         help="The output file name",
         required=False,
-        default=f"{datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d-%I%M%S')}.md",
+        default=f"{output_path}/{datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d-%I%M%S')}.md",
     )
     parser.add_argument(
         "-c",
@@ -79,9 +113,14 @@ if __name__ == "__main__":
         type=str,
         help="Previous output file name to continue the conversation",
         required=False,
-        default="",
+        default=None,
     )
     args = parser.parse_args()
+
+    if not os.path.exists(f"prompts/{args.prompt}.md"):
+        raise FileNotFoundError(f"Prompt file 'prompts/{args.prompt}.md' not found")
+    if args.continue_file and not os.path.exists(args.continue_file):
+        raise FileNotFoundError(f"Continue file '{args.continue_file}' not found")
 
     main(
         openai_key=os.getenv("OPENAI_API_KEY"),
@@ -89,5 +128,5 @@ if __name__ == "__main__":
         model=args.model.strip(),
         prompt=args.prompt.strip(),
         output=args.output.strip(),
-        continue_file=args.continue_file.strip(),
+        continue_file=args.continue_file,
     )
